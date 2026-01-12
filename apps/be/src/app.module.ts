caef522b6dac57c76c5d2cc8b84cb7c0fe3d18be
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-01-12 22:01:07 +0900

    chore: STT 구조 변경에 따른 앱 설정 업데이트 #44

--- apps/be/src/app.module.ts
+++ apps/be/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from './infra/database/database.module';
+import { SessionsModule } from './learning/sessions/sessions.module';
+import { SttModule } from './stt/stt.module';
 
 import { QuestionProviderModule } from './modules/question-provider/question-provider.module';
 
 @Module({
-  imports: [QuestionProviderModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    SessionsModule,
+    SttModule,
+    QuestionProviderModule,
+  ],
   controllers: [],
   providers: [],
 })
--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -2,16 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // DTO 유효성 검사/변환 파이프 글로벌 등록
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
       transform: true,
-      transformOptions: { enableImplicitConversion: true },
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
