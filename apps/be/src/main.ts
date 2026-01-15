212a89e8e6dd24a408712824dd2dbaf6583b1689
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-01-15 20:29:53 +0900

    feat: CORS 설정 #32

--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -10,6 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Talkit API')
