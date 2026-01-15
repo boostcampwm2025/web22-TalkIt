3f68daa266073606681f2bf98b02a2f03503766a
llddang <laliddang@gmail.com>
2026-01-15 18:52:26 +0900

    feat: axios 인스턴스 생성 및 기본 설정 추가 #36

---
+++ apps/fe/src/apis/http.ts
@@ -0,0 +1,17 @@
+import axios, { type AxiosInstance } from 'axios';
+
+const BASE_URL = 'http://localhost:3000/api'; // todo: 추후에 환경변수로 엔드포인트 관리하기
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 30000, // 타임아웃 30초 (개발하면서 적절하게 수정)
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// TODO: [요청 인터셉터] 토큰 주입 등 공통 로직
+
+// TODO: [응답 인터셉터] 에러 공통 처리
+
+export default axiosInstance;
