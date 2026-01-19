dfa631f329ef1cf2053a49fa8a0a0b4eb0ea494c
YeonShin <yeonnim82@gmail.com>
2026-01-19 23:30:58 +0900

    chore: pretendard 로컬 폰트 적용 #54

--- apps/fe/src/main.tsx
+++ apps/fe/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './index.css';
+import 'pretendard/dist/web/variable/pretendardvariable.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
