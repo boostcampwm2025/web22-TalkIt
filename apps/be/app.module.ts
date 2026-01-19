7d76cf261f6b0263407b7cef71b561e29e0b19d0
woohyun365 <woohyun365@naver.com>
2026-01-19 10:39:38 +0900

    refactor: feat: 앱 모듈 및 메인 파일 위치 변경하여 NestJS 애플리케이션 구조 설정

--- apps/be/app.module.ts
+++ apps/be/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { DatabaseModule } from './infra/database/database.module';
-import { SessionsModule } from './learning/sessions/sessions.module';
-import { SttModule } from './stt/stt.module';
-
-import { QuestionProviderModule } from './modules/question-provider/question-provider.module';
-
+import { DatabaseModule } from './src/infra/database/database.module';
+import { SessionsModule } from './src/learning/sessions/sessions.module';
+import { QuestionProviderModule } from './src/modules/question-provider/question-provider.module';
+import { SttModule } from './src/stt/stt.module';
 
 @Module({
   imports: [
