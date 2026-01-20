6c0823724eb6274103e3b63b9cbb6058a68de585
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-01-20 17:54:12 +0900

    feat: SessionsModule에 NormalizeModule 의존성 추가 #45

--- apps/be/src/learning/sessions/sessions.module.ts
+++ apps/be/src/learning/sessions/sessions.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { QuestionProviderModule } from '@/modules/question-provider/question-provider.module';
+import { NormalizeModule } from '@/normalize/normalize.module';
 
 import { DatabaseModule } from '../../infra/database/database.module';
 import { ClovaSttProvider } from '../../stt/providers/clova-stt.provider';
@@ -14,7 +15,7 @@ import { SessionsService } from './services/sessions.service';
 import { SessionsRepository } from './sessions.repository';
 
 @Module({
-  imports: [DatabaseModule, SttModule, QuestionProviderModule],
+  imports: [DatabaseModule, SttModule, QuestionProviderModule, NormalizeModule],
   controllers: [SessionsRecordController, SessionsController],
   providers: [
     SessionsRecordService,
