00aecb1508a43aa397819eff56aa3fb37c7326a8
YeonShin <yeonnim82@gmail.com>
2026-01-27 22:22:33 +0900

    feat: 로그인 비즈니스 로직 및 LocalStrategy 구현 #120 - AuthService: 유저 검증 및 JWT 발급(login) 로직 - LocalStrategy: Passport를 이용한 이메일/비밀번호 검증 전략

--- apps/be/src/auth/auth.module.ts
+++ apps/be/src/auth/auth.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { UsersModule } from '@/users/users.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_ACCESS_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_ACCESS_EXPIRATION_TIME') as any,
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
