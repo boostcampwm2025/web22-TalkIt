6dd665b4699439ee9bb12b302436f9b85c6687f1
woohyun365 <woohyun365@naver.com>
2026-02-03 16:20:00 +0900

    refactor: ZodValidationPipe 제네릭 타입 추가 및 transform 메서드의 반환 타입 정의

--- apps/be/src/common/pipes/zod-validation.pipe.ts
+++ apps/be/src/common/pipes/zod-validation.pipe.ts
@@ -3,10 +3,10 @@ import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { ZodSchema } from 'zod';
 
 @Injectable()
-export class ZodValidationPipe implements PipeTransform {
-  constructor(private readonly schema: ZodSchema) {}
+export class ZodValidationPipe<T = unknown> implements PipeTransform<unknown, T> {
+  constructor(private readonly schema: ZodSchema<T>) {}
 
-  transform(value: unknown) {
+  transform(value: unknown): T {
     const result = this.schema.safeParse(value);
 
     if (!result.success) {
